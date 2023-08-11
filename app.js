19a460e067f1c872417d20e6a7cd80ecd01ca26a
AntonioValentin <cesar_anto30@hotmail.com>
2023-08-11 17:25:43 -0600

    Ingresmos el Switch para la entrada y salida de Información

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const { inquireMenu, pausa } = require('./helpers/inquirer');
+const { inquireMenu, pausa, leerInput } = require('./helpers/inquirer');
+const Tareas = require('./models/Tareas');
+const Tarea = require('./models/tarea');
 
 require('colors')
 
@@ -7,10 +9,25 @@ console.clear();
 const main = async() => {
 
     let opt = ''
+    const tareas = new Tareas();
 
     do {
         
         opt = await inquireMenu();
+
+        switch (opt) {
+            case '1':
+                // Crear Opción
+                const desc = await leerInput('Descripción: ');
+                console.log(desc);
+
+            break;
+            case '2':
+                console.log(tareas.__Listado);
+            break;
+        
+        }
+
         await pausa();
 
     } while (opt != '0');
