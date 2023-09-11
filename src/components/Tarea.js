cdafc4c59d02a2717fbba602049b5d9c379a5998
AntonioValentin <cesar_anto30@hotmail.com>
2023-09-11 16:34:17 -0600

    Componente Hijo para cambiar estado de cada tarea

---
+++ src/components/Tarea.js
@@ -0,0 +1,59 @@
+import React, {useState} from 'react'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheckSquare, faSquare, faEdit, faTimes } from '@fortawesome/free-solid-svg-icons';
+
+const Tarea = ({ tarea, toggleCompleada }) => {
+
+    const [ editandoTarea, cambiarEditandoTarea ] = useState(false)
+    const [ nuevaTarea, cambiarNuevaTarea ] = useState(tarea.texto)
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        cambiarEditandoTarea(false)
+    }
+
+    return (
+        <li
+            className='lista-tareas__tarea'
+        >
+            <FontAwesomeIcon 
+                icon={tarea.completada ? faCheckSquare : faSquare} 
+                className='lista-tareas__icono lista-tareas__icono-check' 
+                onClick={() =>  toggleCompleada(tarea.id) }
+            />
+            <div className='lista-tareas__texto'>
+            {   editandoTarea ? 
+                <form action='' className='formulario-editar-tarea' onSubmit={handleSubmit}>
+                    <input 
+                        type='text'
+                        className='formulario-editar-tarea__input'
+                        value={nuevaTarea}
+                        onChange={(e) => cambiarNuevaTarea(e.target.value)}
+                    />
+                    <button 
+                        type='submit' 
+                        className='formulario-editar-tarea__btn'
+                    >
+                        Actualizar
+                    </button>
+                </form>    
+                : tarea.texto
+            }
+            </div>
+            <div className='lista-tareas__contenedor-botones'>
+                <FontAwesomeIcon 
+                    icon={faEdit} 
+                    className='lista-tareas__icono lista-tareas__icono-accion' 
+                    onClick={() => cambiarEditandoTarea(!editandoTarea)}
+                />
+                <FontAwesomeIcon 
+                    icon={faTimes} 
+                    className='lista-tareas__icono lista-tareas__icono-accion' 
+                />
+            </div>
+        </li>
+    );
+}
+
+export default Tarea;
