f9c181d1a48c8b76652a3a52c49ae8fc1b330c3d
AntonioValentin <cesar_anto30@hotmail.com>
2023-09-12 10:32:00 -0600

    Enviar como referencia los componentes Tareas, ListarTareas

--- src/App.js
+++ src/App.js
@@ -16,13 +16,20 @@ const App = () => {
     ]
   )
 
-  console.log(tareas)
+  const [ mostrarCompletadas, cambiarMostrarCompletadas ] = useState(false)
 
   return (
     <div className='contenedor'>
-      <Header />
+      <Header 
+        mostrarCompletadas={mostrarCompletadas}
+        cambiarMostrarCompletadas={cambiarMostrarCompletadas} 
+      />
       <FormularioTareas tareas={tareas} cambiarTareas={cambiarTareas} />
-      <ListaTareas tareas={tareas} cambiarTareas={cambiarTareas}/>
+      <ListaTareas 
+        tareas={tareas} 
+        cambiarTareas={cambiarTareas}
+        mostrarCompletadas={mostrarCompletadas}
+      />
     </div>
   );
 }
