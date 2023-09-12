50c04a41bf9f062c26a16ef5dcb30258712e9bc6
AntonioValentin <cesar_anto30@hotmail.com>
2023-09-12 10:32:38 -0600

    Funcionandan al boton mostrar tareas

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,34 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
-const Header = () => {
+const Header = ( { mostrarCompletadas, cambiarMostrarCompletadas } ) => {
+
+  const toggleCompletadas = () => {
+    cambiarMostrarCompletadas(!mostrarCompletadas)
+  }
+
   return (
     <header className='header'>
       <h1 className='header__titulo'> Lista de Tareas</h1>
-      <button className='header__boton'>
-        No Mostrar Completadas
-        <FontAwesomeIcon icon={faEyeSlash} className='header__icono-boton' />
-      </button>
+      { 
+        mostrarCompletadas ? 
+        <button 
+          className='header__boton'
+          onClick={ () => toggleCompletadas() }
+        >
+          No Mostrar Completadas
+          <FontAwesomeIcon icon={faEyeSlash} className='header__icono-boton' />
+        </button>
+        :
+        <button 
+          className='header__boton'
+          onClick={ () => toggleCompletadas() }  
+        >
+          Mostrar Completadas
+          <FontAwesomeIcon icon={faEye} className='header__icono-boton' />
+        </button>
+      }
     </header>
   )
 }
